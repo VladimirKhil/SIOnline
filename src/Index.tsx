f25e424b3095156cfe8886c385da46ab042c3113
Valera Bitkovsky <bitvalser@gmail.com>
2020-11-16 22:30:52 +0300

    fix spacing

--- src/Index.tsx
+++ src/Index.tsx
@@ -25,131 +25,129 @@ import ServerInfo from './model/server/ServerInfo';
 declare const config: Config;
 
 function setState(state: State, savedState: SavedState | null, gameId: string | null): State {
-  if (!savedState) {
-    return state;
-  }
+	if (!savedState) {
+		return state;
+	}
 
-  return {
-    ...state,
-    user: {
-      ...state.user,
-      login: savedState.login,
-    },
-    game: savedState.game
-      ? {
-          ...state.game,
-          name: savedState.game.name,
-          password: savedState.game.password,
-          role: savedState.game.role,
-          type: savedState.game.type,
-          playersCount: savedState.game.playersCount,
-        }
-      : state.game,
-    settings: savedState.settings
-      ? {
-          ...state.settings,
-          ...savedState.settings,
-        }
-      : state.settings,
-    online: {
-      ...state.online,
-      selectedGameId: gameId ? parseInt(gameId, 10) : -1,
-    },
-  };
+	return {
+		...state,
+		user: {
+			...state.user,
+			login: savedState.login
+		},
+		game: savedState.game ? {
+			...state.game,
+			name: savedState.game.name,
+			password: savedState.game.password,
+			role: savedState.game.role,
+			type: savedState.game.type,
+			playersCount: savedState.game.playersCount
+		} : state.game,
+		settings: savedState.settings ? {
+			...state.settings,
+			...savedState.settings
+		} : state.settings,
+		online: {
+			...state.online,
+			selectedGameId: gameId ? parseInt(gameId, 10) : -1
+		}
+	};
 }
 
 async function run() {
-  document.title = localization.appTitle;
+	document.title = localization.appTitle;
 
-  if (!config) {
-    throw new Error('Config is undefined!');
-  }
+	if (!config) {
+		throw new Error('Config is undefined!');
+	}
 
-  // Временно до перехода на HTTPS
-  if (config.disableHttps && location.protocol !== 'http:') {
-    location.replace(`http:${location.href.substring(location.protocol.length)}`);
-  }
+	// Временно до перехода на HTTPS
+	if (config.disableHttps && location.protocol !== 'http:') {
+		location.replace(`http:${location.href.substring(location.protocol.length)}`);
+	}
 
-  let serverUri = config.serverUri;
-  if (!serverUri) {
-    const serverDiscoveryUri = config.serverDiscoveryUri;
-    if (!serverDiscoveryUri) {
-      throw new Error('Server uri is undefined!');
-    }
+	let serverUri = config.serverUri;
+	if (!serverUri) {
+		const serverDiscoveryUri = config.serverDiscoveryUri;
+		if (!serverDiscoveryUri) {
+			throw new Error('Server uri is undefined!');
+		}
 
-    const serverUrisResponse = await fetch(serverDiscoveryUri);
-    if (!serverUrisResponse.ok) {
-      throw new Error(`Server discovery is broken! ${serverUrisResponse.status} ${await serverUrisResponse.text()}`);
-    }
+		const serverUrisResponse = await fetch(serverDiscoveryUri);
+		if (!serverUrisResponse.ok) {
+			throw new Error(`Server discovery is broken! ${serverUrisResponse.status} ${await serverUrisResponse.text()}`);
+		}
 
-    const serverUris = (await serverUrisResponse.json()) as ServerInfo[];
-    if (!serverUris || serverUris.length === 0) {
-      throw new Error('Server uris object is broken!');
-    }
+		const serverUris = (await serverUrisResponse.json()) as ServerInfo[];
+		if (!serverUris || serverUris.length === 0) {
+			throw new Error('Server uris object is broken!');
+		}
 
-    serverUri = serverUris[0].uri;
-  }
+		serverUri = serverUris[0].uri;
+	}
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const gameId = urlParams.get('gameId');
+	const urlParams = new URLSearchParams(window.location.search);
+	const gameId = urlParams.get('gameId');
 
-  const savedState = loadState();
-  const state = setState(initialState, savedState, gameId);
+	const savedState = loadState();
+	const state = setState(initialState, savedState, gameId);
 
-  const dataContext: DataContext = {
-    config,
-    serverUri,
-    connection: null,
-    contentUris: null,
-  };
+	const dataContext: DataContext = {
+		config,
+		serverUri,
+		connection: null,
+		contentUris: null
+	};
 
-  const store = createStore<State, KnownAction, {}, {}>(
-    reducer,
-    state,
-    applyMiddleware(reduxThunk.withExtraArgument(dataContext))
-  );
+	const store = createStore<State, KnownAction, {}, {}>(
+		reducer,
+		state,
+		applyMiddleware(reduxThunk.withExtraArgument(dataContext))
+	);
 
-  let currentSettings = state.settings;
-  store.subscribe(() => {
-    const newState = store.getState();
-    const newSettings = newState.settings;
-    if (newSettings !== currentSettings) {
-      currentSettings = newSettings;
-      actionCreators.saveStateToStorage(newState);
-    }
-  });
+	let currentSettings = state.settings;
+	store.subscribe(() => {
+		const newState = store.getState();
+		const newSettings = newState.settings;
+		if (newSettings !== currentSettings) {
+			currentSettings = newSettings;
+			actionCreators.saveStateToStorage(newState);
+		}
+	});
 
-  subscribeToExternalEvents(store);
+	subscribeToExternalEvents(store);
 
-  ReactDOM.render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <App ads={config.ads} />
-      </Provider>
-    </BrowserRouter>,
-    document.getElementById('reactHost')
-  );
+	ReactDOM.render(
+		(
+			<BrowserRouter>
+				<Provider store={store}>
+					<App ads={config.ads} />
+				</Provider>
+			</BrowserRouter>
+		),
+		document.getElementById('reactHost')
+	);
 
-  store.dispatch(actionCreators.navigateToLogin());
+	store.dispatch(actionCreators.navigateToLogin());
 }
 
 function subscribeToExternalEvents(store: Store<State, any>) {
-  window.onresize = () => store.dispatch(actionCreators.windowWidthChanged(window.innerWidth));
-  window.onpopstate = () => true;
+	window.onresize = () => store.dispatch(actionCreators.windowWidthChanged(window.innerWidth));
+	window.onpopstate = () => true;
 
-  window.onkeydown = (e: KeyboardEvent) => {
-    if (e.keyCode === Constants.KEY_CTRL) {
-      store.dispatch(runActionCreators.pressGameButton());
-    }
+	window.onkeydown = (e: KeyboardEvent) => {
+		if (e.keyCode === Constants.KEY_CTRL) {
+			store.dispatch(runActionCreators.pressGameButton());
+		}
 
-    return true;
-  };
+		return true;
+	};
 
-  window.oncontextmenu = (e: MouseEvent) => {
-    store.dispatch(runActionCreators.pressGameButton());
-    e.preventDefault();
-    return true;
-  };
+	window.oncontextmenu = (e: MouseEvent) => {
+		store.dispatch(runActionCreators.pressGameButton());
+		e.preventDefault();
+		return true;
+	};
 }
 
 run();
