d639b2329aeb8bb1f09afc4f52da2c26e488a243
m1x0n <mmorozovm@gmail.com>
2023-04-08 13:26:31 +0300

    Get use of localization

--- src/components/common/ChatInputEmojiPicker.tsx
+++ src/components/common/ChatInputEmojiPicker.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { useState } from 'react';
 import EmojiPicker, { EmojiClickData, EmojiStyle, SkinTones } from 'emoji-picker-react';
+import localization from '../../model/resources/localization';
 import './ChatInputEmojiPicker.css';
 
 interface ChatInputEmojiPickerProps {
 	onEmojiClick: (emojiData: EmojiClickData, e: MouseEvent) => void;
 }
 export default function ChatInputEmojiPicker(props: ChatInputEmojiPickerProps): JSX.Element {
-	const BUTTON_HINT = 'Pick an emoji';
 	const [isEmojiPickerOpened, setIsEmojiPickerOpened] = useState(false);
 
 	const onEmojiPickerButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -20,7 +20,7 @@ export default function ChatInputEmojiPicker(props: ChatInputEmojiPickerProps): 
 				type={'button'}
 				className={'standard chatEmojiPickerButton'}
 				onClick={onEmojiPickerButtonClick}
-				title={BUTTON_HINT}
+				title={localization.pickAnEmoji}
 			>🙂
 			</button>
 
