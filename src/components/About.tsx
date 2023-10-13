84f1a42b12a002cbf8024c7447b7b5caba40004e
VladimirKhil <vladimir.khil@gmail.com>
2023-10-13 08:17:50 +0200

    Update About dialog: add more authors info and donate links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -25,17 +25,32 @@ export function About(props: AboutProps): JSX.Element {
 				<section>
 					{localization.about.map(text => (<p key={text}>{text}</p>))}
 				</section>
+
 				<section>
 					<header><h1 className="subHeader">{localization.aboutSupport}</h1></header>
 					<p><a href="https://vk.com/topic-135725718_34967839">{localization.supportInfo}</a></p>
 				</section>
+
 				<section>
-					<header><h1 className="subHeader">{localization.aboutAuthorLicense}</h1></header>
-					<p>{localization.authorInfo}</p>
+					<header><h1 className="subHeader">{localization.aboutAuthor}</h1></header>
+					<p>{localization.gameAuthor}: <a href="https://vladimirkhil.com">{localization.authorInfo}</a>.</p>
+					<p>{localization.designer}: {localization.designerInfo}.</p>
+					<p>{localization.composer}: <a href="https://soundcloud.com/vladislav-hoshenko">Vlad Hoshenko</a>.</p>
 					<p><a href="https://github.com/VladimirKhil/SIOnline">{localization.sourcesInfo}</a></p>
-					<p>{localization.licence}</p>
+				</section>
+
+				<section>
+					<header><h1 className='subHeader'>{localization.donate}</h1></header>
+					<p><a href='https://www.patreon.com/vladimirkhil'>Patreon</a></p>
+					<p><a href='https://boosty.to/vladimirkhil'>Boosty</a></p>
+				</section>
+
+				<section>
+					<header><h1 className='subHeader'>{localization.license}</h1></header>
+					<p>{localization.licenseInfo}</p>
 					<p>{localization.noWarranty}</p>
 					<p>{localization.usedComponents}</p>
+
 					<ul className="components">
 						<li><a href="https://github.com/facebook/react/blob/master/LICENSE">React, React DOM (MIT)</a></li>
 						<li><a href="https://github.com/reduxjs/redux/blob/master/LICENSE.md">Redux (MIT)</a></li>
@@ -45,11 +60,13 @@ export function About(props: AboutProps): JSX.Element {
 						<li><a href="https://github.com/stefanpenner/es6-promise/blob/master/LICENSE">ES6 Promise (MIT)</a></li>
 						<li><a href="https://github.com/Stuk/jszip/blob/master/LICENSE.markdown">JSZip (MIT)</a></li>
 						<li><a href="https://github.com/srijs/rusha/blob/master/LICENSE">Rusha (MIT)</a></li>
+
 						<li>
 							<a href="https://github.com/dotnet/aspnetcore/blob/master/LICENSE.txt">
 								Microsoft SignalR, Microsoft SignalR MessagePack (Apache 2.0)
 							</a>
 						</li>
+
 						<li><a href="https://github.com/richtr/NoSleep.js/blob/master/LICENSE">NoSleep.JS (MIT)</a></li>
 					</ul>
 				</section>
